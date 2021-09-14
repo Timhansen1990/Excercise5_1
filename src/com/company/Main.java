91b8795f790ab31fa3999b6ba4a4c7e545340a4e
tim Hansen <timx1776@edu.easj.dk>
2021-09-14 15:02:09 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x = 9;
+
+        if (x > 0 && x < 10){
+            System.out.println("Positive single digit number");
+        }
+    }
+}
